b36616ed62f3a7308b952e0542169de978a52e05
Dcastillomar <125750537+Dcastillomar@users.noreply.github.com>
2023-11-07 17:23:28 -0600

    update connection.php

--- connection.php
+++ connection.php
@@ -1,25 +1,7 @@
 <?php
-// $dbhost = "localhost";
-// $dbuser = "root";
-// $dbpass = "";
-// $dbname = "signupdb"; 
+$dbhost = "wb39lt71kvkgdmw0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$dbuser = "g5rw9okqdy8cyfac";
+$dbpass = "hlkbe5whuwap778k";
+$dbname = "kcy1de553ln3xoyt"; 
 
-// if (!$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname)) {
-//     die("failed to connect");
-// }
-
-$url = getenv('JAWSDB_URL');
-$dbparts = parse_url($url);
-
-$hostname = $dbparts['wb39lt71kvkgdmw0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	'];
-$username = $dbparts['g5rw9okqdy8cyfac	'];
-$password = $dbparts['hlkbe5whuwap778k'];
-$database = ltrim($dbparts['kcy1de553ln3xoyt'],'/');
-
-$conn = new mysqli($hostname, $username, $password, $database);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connection was successfully established!";
+$connect = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die("database connection error");
